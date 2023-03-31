44367cde3cd1397c039568b3a301b2df9aa7e821
Dave <109882672+daveskura@users.noreply.github.com>
2023-03-31 14:58:23 -0400

    upadtes

--- run.bat
+++ run.bat
@@ -14,6 +14,6 @@ echo py -m twine upload --repository-url http://localhost:8080 dist/*
 echo.
 echo pip search package --extra-index-url http://localhost:8080
 echo.
-echo pip install schemawizard-package==2.3.2 --extra-index-url http://localhost:8080
+echo pip install zetl==2.0.3 --extra-index-url http://localhost:8080
 echo.
 call pypi-server -p 8080 ./packages -P . -a .
