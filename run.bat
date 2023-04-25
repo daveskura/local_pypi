82d1727c950998f1f304d18dd7e3dfb35b666c92
Dave <109882672+daveskura@users.noreply.github.com>
2023-04-25 10:15:52 -0400

    updates

--- run.bat
+++ run.bat
@@ -14,7 +14,6 @@ echo py -m twine upload --repository-url http://localhost:8080 dist/*
 echo.
 echo pip search package --extra-index-url http://localhost:8080
 echo.
-echo pip install schemawizard_package==2.3.9 --extra-index-url http://localhost:8080
+echo pip install schemawizard_package==2.4.0 --extra-index-url http://localhost:8080
 echo.
-echo call pypi-server --overwrite -p 8080 ./packages -P . -a .
-call pypi-server -p 8080 ./packages -P . -a .
+call pypi-server --overwrite -p 8080 ./packages -P . -a .
