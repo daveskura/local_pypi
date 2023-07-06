45a147c790a041b902d0b2b2109be2810232e702
Dave <109882672+daveskura@users.noreply.github.com>
2023-07-06 09:02:57 -0400

    Revert "updates"
    This reverts commit 0dc47f6883f092d9e06ecf9728d3bcf0b21f3091.

--- run.bat
+++ run.bat
@@ -14,7 +14,7 @@ echo py -m twine upload --repository-url http://localhost:8080 dist/*
 echo.
 echo pip search package --extra-index-url http://localhost:8080
 echo.
-echo pip install sqlman==1.0.0 --extra-index-url http://localhost:8080
+echo pip install dbx==1.0.2 --extra-index-url http://localhost:8080
 echo.
 call pypi-server --overwrite -p 1972 ./packages -P . -a .
 
